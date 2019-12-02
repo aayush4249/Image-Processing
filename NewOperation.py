scaling fix; scale factors

NewOperation.py
@@ -5,8 +5,8 @@ import cv2
 
 def main():
 
-    img = cv2.imread("Images/noisy2.jpg", 0) 
-    img2 = cv2.imread("Images/dog.png",0) 
+    img = cv2.imread("Images/nice.jpg", 0) 
+    img2 = cv2.imread("Images/quote.jpg", 0) 
 
 
     img_sized = cv2.resize(img, (400, 400))
@@ -19,13 +19,13 @@ def main():
         for j in range(400):
             combined[i][j] = img_sized[i][j] & img2_sized[i][j]
     
-    final = reduceNoise(img)
-    cv2.imwrite("Images/combined.jpg",final)
+    final = reduceNoise(combined)
+    cv2.imwrite("Images/combined.jpg", final)
     
     
     img3 = cv2.imread("Images/combined.jpg")
     
-    cv2.imshow('image', img3)
+    cv2.imshow('Combined Image', img3)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
@@ -34,8 +34,8 @@ def main():
 def reduceNoise(img):
     
     #Uses a 3x3 filter to remove noise from the image
-    row,col = img.shape 
-    newImg = np.zeros((row,col))
+    row, col = img.shape 
+    newImg = np.zeros((row, col))
     
     for i in range(1, row - 2):
         
@@ -44,10 +44,10 @@ def reduceNoise(img):
             win = []
             
             #Goes through 3 rows
-            for x in range(i-2,i+3):
+            for x in range(i - 2, i + 3):
                 
                 #Goes through 3 columns
-                for y in range(j-2,j+3):
+                for y in range(j - 2, j + 3):
                     
                     win.append(img[x][y])
             

scaling.py
@@ -62,7 +62,7 @@ def DFS(i, j, visited, image):
 
 
 
-def checkBounds(i,j,visited,image):
+def checkBounds(i, j, visited, image):
 
     #check if row and column are in range
     #check to ensure the pixel has not been visited yet
@@ -78,26 +78,28 @@ def average(x, y):
     return (x + y) / 2
 
 # scales an image down by half
-def minifyScaleByTwo(image):
+def minifyScale(image, scale_factor):
 
     # get image dimensions
     height, width  = image.shape
-    tHeight = height / 2
-    tWidth = width / 2
+    tHeight = height / scale_factor
+    tWidth = width / scale_factor
 
     # create output image
     output = np.zeros((tHeight, tWidth), np.uint8)
     #output = cv2.resize(output, (tWidth, tHeight))
 
-    for x in range(0, tHeight):
-        x2 = x * 2
-        print("x2", x2)
-        for y in range(0, tWidth):
-            y2 = y * 2
-            print("y2:", y2)
-            p = average(image[x2][y2], image[x2][y2 + 1])
-            q = average(image[x2 + 1][y2], image[x2 + 1][y2 + 1])
-            output[x][y] = average(p, q)
+    for i in range(0, tHeight):
+        x = i * scale_factor
+        print("i, x", i, x)
+        for j in range(0, tWidth):
+            y = j * scale_factor
+
+            # add the average of a group of 4 pixels to the output image
+            output[i][j] = average(
+                average(image[x][y], image[x][y + 1]),
+                average(image[x + 1][y], image[x + 1][y + 1])
+            )
 
     output = binary(output)
     print(output)
@@ -106,16 +108,10 @@ def minifyScaleByTwo(image):
 def main():
 
     img = cv2.imread('images/bird.jpg', cv2.IMREAD_GRAYSCALE)
-    # img2 = (np.array([
-    #         [1, 1, 0, 0, 0], 
-    #         [0, 1, 0, 0, 1], 
-    #         [1, 0, 0, 1, 1], 
-    #         [0, 0, 0, 0, 0], 
-    #         [1, 0, 1, 0, 1]
-    #     ]))
     
     out = binary(img)
-    out = minifyScaleByTwo(img)
+    out = minifyScale(img, 2)
+    cv2.imwrite("Images/scaled2.jpg", out)
     cv2.imshow('image', out)
     cv2.waitKey(0)
     cv2.destroyAllWindows()